700c5a8076479054cd04f718a264fa210e584d6b
KB-Designs <kimanibrian030@gmail.com>
2026-03-04 11:49:25 +0300

    ,,

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -45,7 +45,7 @@ func main() {
 		os.Getenv("MPESA_KEY"),
 		os.Getenv("MPESA_SECRET"),
 	)
-	mpesaService.Config.CallbackURL = "https://7352-102-213-49-75.ngrok-free.app/api/callback/mpesa"
+	mpesaService.Config.CallbackURL = "https://crave-and-glaze-final.onrender.com/api/callback/mpesa"
 
 	mailService := mailer.New(
 		os.Getenv("SMTP_HOST"),
